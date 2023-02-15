e3d159545cc168ba4259bd0650d6ebd66f9e3fcb
Thiago Aidar Figueiredo <32144547@educa.mackenzie.br>
2023-02-15 08:42:20 -0300

    Lab1A- Hello World!

---
+++ Lab1A.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+Luiz Octavio Tassinari Saraiva - 32030411
+Thiago Aidar Figueiredo -32144547
+Yiou Wu - 32123213
+**/
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
+
