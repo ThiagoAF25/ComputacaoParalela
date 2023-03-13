a1ab55a496bc4bc82d2cef190f01f6c36e3fbf58
Ubuntu <ubuntu@ip-172-31-8-53.ec2.internal>
2023-03-13 22:29:46 +0000

    ex7 adicionado

---
+++ Lab1B/ex7.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/*
+Luiz Octavio Tassinari Saraiva
+Thiago Aidar Figueiredo
+Yiou Wu
+*/
+
+        struct pessoa{
+                char nome[20];
+                int idade;
+                float peso;
+                float altura;
+        };
+
+
+        int main(){
+                struct pessoa Pessoas[3];
+                for(int i=0; i<3; i++){
+                        printf("Digite o nome da %d° pessoa: ", i+1);
+                        scanf("%s", Pessoas[i].nome);
+                        printf("Digite a idade da %d° pessoa: ", i+1);
+                        scanf("%d", &Pessoas[i].idade);
+                        printf("Digite o peso da %d° pessoa: ", i+1);
+                        scanf("%f", &Pessoas[i].peso);
+                        printf("Digite o altura da %d° pessoa: ", i+1);
+                        scanf("%f", &Pessoas[i].altura);
+                }
+
+                for(int i=0; i<3; i++){
+                        printf("Nome: %s\n", Pessoas[i].nome);
+                        printf("Idade: %d\n", Pessoas[i].idade);
+                        printf("Peso: %.2f\n", Pessoas[i].peso);
+                        printf("Altura: %.2f\n", Pessoas[i].altura);
+                }
+                printf("\n");
+                return 0;
+        }
