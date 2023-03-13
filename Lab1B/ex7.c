4cb8381eb3e7e33ffe799628a5686c69825d0486
Ubuntu <ubuntu@ip-172-31-49-211.ec2.internal>
2023-03-13 22:22:27 +0000

    ex7 adicionado sucesso

--- Lab1B/ex7.c
+++ Lab1B/ex7.c
@@ -6,33 +6,33 @@ Thiago Aidar Figueiredo
 Yiou Wu
 */
 
-	struct pessoa{
-  		char nome[20];
-  		int idade;
-  		float peso;
-  		float altura;
-	};
+        struct pessoa{
+                char nome[20];
+                int idade;
+                float peso;
+                float altura;
+        };
 
 
-	int main(){
-  		struct pessoa Pessoas[3];
-  		for(int i=0; i<3; i++){
-    			printf("Digite o nome da %d° pessoa: ", i+1);
-    			scanf("%s", Pessoas[i].nome);
-    			printf("Digite a idade da %d° pessoa: ", i+1);
-    			scanf("%d", &Pessoas[i].idade);
-    			printf("Digite o peso da %d° pessoa: ", i+1);
-    			scanf("%f", &Pessoas[i].peso);
-    			printf("Digite o altura da %d° pessoa: ", i+1);
-    			scanf("%f", &Pessoas[i].altura);
-  		}
+        int main(){
+                struct pessoa Pessoas[3];
+                for(int i=0; i<3; i++){
+                        printf("Digite o nome da %d° pessoa: ", i+1);
+                        scanf("%s", Pessoas[i].nome);
+                        printf("Digite a idade da %d° pessoa: ", i+1);
+                        scanf("%d", &Pessoas[i].idade);
+                        printf("Digite o peso da %d° pessoa: ", i+1);
+                        scanf("%f", &Pessoas[i].peso);
+                        printf("Digite o altura da %d° pessoa: ", i+1);
+                        scanf("%f", &Pessoas[i].altura);
+                }
 
-  		for(int i=0; i<3; i++){
-    			printf("Nome: %s\n", Pessoas[i].nome);
-    			printf("Idade: %d\n", Pessoas[i].idade);
-    			printf("Peso: %.2f\n", Pessoas[i].peso);
-    			printf("Altura: %.2f\n", Pessoas[i].altura);
-  		}
-		printf("\n");
-		return 0;
-	}
+                for(int i=0; i<3; i++){
+                        printf("Nome: %s\n", Pessoas[i].nome);
+                        printf("Idade: %d\n", Pessoas[i].idade);
+                        printf("Peso: %.2f\n", Pessoas[i].peso);
+                        printf("Altura: %.2f\n", Pessoas[i].altura);
+                }
+                printf("\n");
+                return 0;
+        }
