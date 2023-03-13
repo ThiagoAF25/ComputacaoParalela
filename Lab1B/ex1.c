ccbd18ff7b32c4b5644dad05038b7c525062c4c4
Ubuntu <ubuntu@ip-172-31-8-53.ec2.internal>
2023-03-13 21:21:16 +0000

    alterações no print do exercício 1

--- Lab1B/ex1.c
+++ Lab1B/ex1.c
@@ -4,22 +4,24 @@
    Yiou Wu
 */
 int menorMatriz(int matriz[3][3]){
-    int linhamenor, menor = matriz[0][0]; //linhamenor guarda linha com o menor número e menor o menor número
-    for(int i = 0; i < sizeof(matriz[0][0])-1;i++){ //Verificando linha a linha
-       for(int j = 0; j < sizeof(matriz[0][0])-1;j++){ //Verificando coluna a coluna
-           if (matriz[i][j]<menor){ //Se o número da posição for menor, troca linhamenor(linha que possui o menor número) e menor
-              linhamenor = i;
-              menor = matriz[i][j];
-           }
-       }
-    }
-    return linhamenor;
+    	int linhamenor, menor = matriz[0][0]; //linhamenor guarda linha com o menor número e menor o menor número
+    	for(int i = 0; i < sizeof(matriz[0][0])-1;i++){ //Verificando linha a linha
+       		for(int j = 0; j < sizeof(matriz[0][0])-1;j++){ //Verificando coluna a coluna
+           		printf("%d ", matriz[i][j]);
+	   		if (matriz[i][j]<menor){ //Se o número da posição for menor, troca linhamenor(linha que possui o menor número) e menor
+              			linhamenor = i;
+              			menor = matriz[i][j];
+           		}
+       		}
+		printf("\n");
+    	}
+    	return linhamenor;
 }
 
 int main()
 {
-    int matriz[3][3]= {{4,5,6},{10,1,6},{9,8,7}};
-    printf("Linha: %d\n",menorMatriz(matriz));
+    	int matriz[3][3]= {{4,5,6},{10,1,6},{9,8,7}};
+    	printf("Linha: %d\n",menorMatriz(matriz));
 
-    return 0;
+    	return 0;
 }
