8da78092baf7202bbd54f6c837a2cd97cb09e8ba
Ubuntu <ubuntu@ip-172-31-6-53.ec2.internal>
2023-05-08 13:06:24 +0000

    melhorias no codigo serial e novo codigo paralelo

--- projetofinal/serial.c
+++ projetofinal/serial.c
@@ -4,26 +4,21 @@
 
 int main(void) {
   int i=1, n = 3250;
-  mpf_t euler, fatorial, var, res;
+  mpf_t euler, res;
   mpf_init2(euler, 33500U);
-  mpf_init2(fatorial, 33500U);
-  mpf_init2(var, 33500U);
   mpf_init2(res, 33500U);
   mpf_set_ui(euler,1);
-  mpf_set_ui(fatorial,1);
-  mpf_set_ui(var, 1);
   mpf_set_ui(res, 1);
 
   while(i <= n){
-    mpf_mul_ui(fatorial, fatorial, i);
-    mpf_div(res, var, fatorial);
+    mpf_div_ui(res, res, i);
     mpf_add(euler, euler, res);
     i++;
   }
   gmp_printf("%.9999Ff\n", euler);
 
   mpf_clear(euler);
-  mpf_clear(fatorial);
+  mpf_clear(res);
 
   return 0;
 }
