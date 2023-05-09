0b10274b82f288b8cbcf7b2cc7bcc9bd4a0eb871
Ubuntu <ubuntu@ip-172-31-14-204.ec2.internal>
2023-05-09 20:28:41 +0000

    Resolução em paralelo para multiplas threads

--- projetofinal/paraleloNThreads.c
+++ projetofinal/paraleloNThreads.c
@@ -6,47 +6,45 @@
 void thread_Euler(int n, mpf_t *global_euler);
 
 int main(int agrc , char*argv[]) {
-  int thread_count, n = 3250;
-  mpf_t global_euler;
-  mpf_init2(global_euler, 33500U);
-  thread_count = strtol(argv[1],NULL, 10);
+	FILE *fptr;
+	int thread_count, n = 125000;
+  	mpf_t global_euler;
+  	mpf_init2(global_euler, 3350000U);
+	mpf_set_ui(global_euler,1);
+  	thread_count = strtol(argv[1],NULL, 10);
 #pragma omp parallel num_threads(thread_count)
 
-  thread_Euler(n,  &global_euler);
-  printf("Euler: ");
-  
-  gmp_printf("%.9999Ff\n", global_euler);
+  	thread_Euler(n,  &global_euler);
+  	fptr = fopen("ResultadosNThreads.txt","w");
+  	gmp_fprintf(fptr,"%.9999Ff\n", global_euler);
 
-  mpf_clear(global_euler);
+  	mpf_clear(global_euler);
 
 
-  return 0;
+  	return 0;
 }
 
 void thread_Euler(int n, mpf_t *global_euler){
-  mpf_t fatorial, var, res, local_euler;
-  int my_rank = omp_get_thread_num();
-  int thread_count = omp_get_num_threads();
-  long double mult_fatorial;
-  mpf_init2(fatorial, 33500U);
-  mpf_init2(var, 33500U);
-  mpf_init2(res, 33500U);
-  mpf_init2(local_euler, 33500U);
-  mpf_set_ui(local_euler,1);
-  mpf_set_ui(fatorial,1);
-  mpf_set_ui(var, 1);
-  mpf_set_ui(res, 1);
-  for(int i = my_rank+2; i <= n; i+=thread_count){
-    mult_fatorial = i*(i-1);
-    mpf_mul_ui(fatorial, fatorial, mult_fatorial);
-    mpf_div(res, var, fatorial);
-    mpf_add(local_euler, local_euler, res);
-  }
+  	mpf_t res, local_euler;
+  	int my_rank = omp_get_thread_num();
+  	int thread_count = omp_get_num_threads();
+  	mpf_init2(res, 33500U);
+  	mpf_init2(local_euler, 33500U);
+  	mpf_set_ui(local_euler,0);
+  	mpf_set_ui(res, 1);
+	int start = (n/thread_count)*my_rank; 
+	int local_n = (n/thread_count)*(my_rank+1);
+  	for(int i = 1; i <= local_n; i++){
+    		if(i > start){
+      			mpf_div_ui(res, res, i);
+      			mpf_add(local_euler, local_euler, res);
+    		}else{
+      			mpf_div_ui(res, res, i);
+    		}
+  	}
 #   pragma omp critical
-  mpf_add(*global_euler, *global_euler, local_euler);
+  	mpf_add(*global_euler, *global_euler, local_euler);
 
-  mpf_clear(local_euler);
-  mpf_clear(fatorial);
-  mpf_clear(var);
-  mpf_clear(res);
+  	mpf_clear(local_euler);
+  	mpf_clear(res);
 }
