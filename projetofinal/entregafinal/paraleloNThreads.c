3b8a1959b633cc9ceacb8dc9cd70d77343891616
ThiagoAF25 <thiago.figueiredo2509@gmail.com>
2023-05-28 17:00:29 -0300

    Adicionando arquivos finais

---
+++ projetofinal/entregafinal/paraleloNThreads.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
+#include <omp.h>
+
+void thread_Euler(int n, mpf_t *global_euler);
+
+int main(int agrc , char*argv[]) {
+	FILE *fptr;
+	int thread_count, n = 400000;
+  	mpf_t global_euler;
+  	mpf_init2(global_euler, 6700000U);
+	mpf_set_ui(global_euler,1);
+  	thread_count = strtol(argv[1],NULL, 10);
+#pragma omp parallel num_threads(thread_count)
+
+  	thread_Euler(n,  &global_euler);
+  	fptr = fopen("ResultadosNThreads.txt","w");
+  	gmp_fprintf(fptr,"%.1999999Ff\n", global_euler);
+
+  	mpf_clear(global_euler);
+
+
+  	return 0;
+}
+
+void thread_Euler(int n, mpf_t *global_euler){
+  	mpf_t res, local_euler;
+  	int my_rank = omp_get_thread_num();
+  	int thread_count = omp_get_num_threads();
+  	mpf_init2(res, 6700000U);
+  	mpf_init2(local_euler, 6700000U);
+  	mpf_set_ui(local_euler,0);
+  	mpf_set_ui(res, 1);
+	int start = (n/thread_count)*my_rank; 
+	int local_n = (n/thread_count)*(my_rank+1);	
+	int i;
+    	for(i = 1; i <= start; i++){
+              mpf_div_ui(res, res, i);
+	}
+    	for(i = start+1; i <= local_n; i++){
+              mpf_div_ui(res, res, i);
+              mpf_add(local_euler, local_euler, res);
+    	}
+#   pragma omp critical
+  	mpf_add(*global_euler, *global_euler, local_euler);
+
+  	mpf_clear(local_euler);
+  	mpf_clear(res);
+}
---
+++ projetofinal/entregafinal/serial.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#include <gmp.h>
+
+int main(void) {
+  FILE *fptr;
+  int i=1, n = 400000;
+  mpf_t euler, res;
+  mpf_init2(euler, 6700000U);
+  mpf_init2(res, 6700000U);
+  mpf_set_ui(euler,1);
+  mpf_set_ui(res, 1);
+
+  while(i <= n){
+    mpf_div_ui(res, res, i);
+    mpf_add(euler, euler, res);
+    i++;
+  }
+  fptr = fopen("ResultadosSerial.txt","w");
+
+  gmp_fprintf(fptr,"%.1999999Ff\n", euler);
+
+  mpf_clear(euler);
+  mpf_clear(res);
+  fclose(fptr);
+  return 0;
+}
+
